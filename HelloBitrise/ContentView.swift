f9b0f9a0268c78d582b9fb9d89153007edb8cbd8
Jirat Ki <jirat.ki@wework.com>
2020-06-07 01:43:24 +0800

    Initial Commit

---
+++ HelloBitrise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloBitrise
+//
+//  Created by jirat.ki on 7/6/20.
+//  Copyright © 2020 jirat.ki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
